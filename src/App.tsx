e0e1fe187711519b812b12ebce8a74076fa19a38
Selim Yesilkaya <selimyesilkaya@gmail.com>
2021-09-05 00:14:12 +0300

    clean up unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,22 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import * as React from "react";
+import { ChakraProvider, Box, theme, Center, Input, Button, Flex, Text, Heading } from "@chakra-ui/react";
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+    <ChakraProvider theme={theme}>
+        <Center w="100vw" h="100vh">
+            <Flex borderRadius="md" w="90%" h="90%" bg="gray.100" p="2">
+                <Flex w="100%" direction="column" m="2" gridGap="2">
+                    <Flex w="100%" gridGap="2">
+                        <Input name="message" colorScheme="gray" bg="gray.300" placeholder="Enter message here" />
+                        <Button colorScheme="green">Send</Button>
+                    </Flex>
+                    <Flex bg="gray.400" borderRadius="md">
+                        <Heading size="md" m="1">
+                            Received Messages
+                        </Heading>
+                    </Flex>
+                </Flex>
+            </Flex>
+        </Center>
+    </ChakraProvider>
+);
